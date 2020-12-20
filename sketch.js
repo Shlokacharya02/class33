8cc23181cb9b69c58d0c8c535395686b6a06e641
shlok <yogeshacharya21966@gmail.com>
2020-12-20 12:18:14 +0530

    fixed the issues

--- sketch.js
+++ sketch.js
@@ -95,7 +95,9 @@ function mouseReleased(){
 
 function keyPressed(){
     if(keyCode === 32){
-       // slingshot.attach(bird.body);
+        bird.trajectory=[]
+        Matter.Body.setPosition(bird.body,{x:200,y:50})
+       slingshot.attach(bird.body);
     }
 }
 
